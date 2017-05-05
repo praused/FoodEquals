261ccfa9a67df343f90ea7825f2fe62f9f21dcea
David Prause <praused@hotmail.com>
2017-05-05 10:29:13 -0400

    Equiality in .Net, The Virtual Object.Equals() Method

--- FoodEquals/Program.cs
+++ FoodEquals/Program.cs
@@ -10,6 +10,12 @@ namespace FoodEquals
     {
         static void Main(string[] args)
         {
+            Food banana = new Food("banana");
+            Food banana2 = new Food("banana");
+            Food chocolate = new Food("chocolate");
+
+            Console.WriteLine(banana.Equals(chocolate));
+            Console.WriteLine(banana.Equals(banana2));
         }
     }
 }
